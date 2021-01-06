33b3f2c9a9197505cc41291bb6ce889a6aed2e3d
huzz <605198347@qq.com>
2021-01-06 15:13:53 +0800

    提交一起执行多个spring boot的run.jar

---
+++ src/main/java/com/hzz/hzzcloud/test/类中静态类/test1.java
@@ -0,0 +1,16 @@
+package com.hzz.hzzcloud.test.类中静态类;
+
+/**
+ * @author ：hzz
+ * @description：TODO
+ * @date ：2021/1/6 11:56
+ */
+public class test1 {
+
+
+    public static void main(String[] args) {
+
+        staticTest staticTest = new staticTest();
+        com.hzz.hzzcloud.test.类中静态类.staticTest.cla1 cla1 = staticTest.getcla1();
+    }
+}
