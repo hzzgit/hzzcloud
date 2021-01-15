7a0d302afdb7592f5aa7d1a21b6a7d8382c54e28
huzz <605198347@qq.com>
2021-01-15 15:10:54 +0800

    手动封装RestTemplat的get、post表单、postjson

--- src/main/java/com/hzz/hzzcloud/controller/测试熔断机制/HystiryController.java
+++ src/main/java/com/hzz/hzzcloud/controller/测试熔断机制/HystiryController.java
@@ -3,9 +3,19 @@ package com.hzz.hzzcloud.controller.测试熔断机制;
 import com.hzz.hzzcloud.OpenFeign.MonitorClient;
 import com.hzz.hzzcloud.OpenFeign.vo.GetDepTreeVO;
 import com.hzz.hzzcloud.OpenFeign.vo.JsonMessage;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ：hzz
@@ -14,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("hystiry")
-public class HystiryController {
+public class HystiryController extends RestTemplateApi {
 
     @Autowired
     private MonitorClient monitorClient;
@@ -26,5 +36,28 @@ public class HystiryController {
         getDepTreeVO.setPageSize(100);
         getDepTreeVO.setState(0);
         return monitorClient.getStores(getDepTreeVO);
+    }
+
+    /**
+     * 获取车辆列表
+     * @return
+     */
+    @RequestMapping("/getonlinevehicle.action")
+    public Object getonlinevehicle(){
+        Map<String,Object> param=new HashMap<>();
+        param.put("rows",10);
+        param.put("page",2);
+        GetDepTreeVO pageable=new GetDepTreeVO();
+        pageable.setPageSize(30);
+       // String post = Post("", 10, param);
+       // String post2 = PostJson("", 10, pageable);
+        String get = get("http://monitorwebapice1/realData/getonlinevehicle.action", 10, param);
+
+        return get;
+    }
+
+    public static void main(String[] args) {
+        String s = DigestUtils.md5Hex("123456abc");
+        System.out.println(s);
     }
 }
