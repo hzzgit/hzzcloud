c016ef61411b8b2aff8c8f8ca9e8304bafa78707
huzz <605198347@qq.com>
2021-02-03 18:02:13 +0800

    二维数组和反射

---
+++ src/main/resources/templatesdir/main/common/flowlimitconfig/controller/FlowlimitconfigController.java
@@ -0,0 +1,279 @@
+package common.flowlimitconfig.controller;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import com.ltmonitor.web.util.quanxianUtil;
+import com.ltmonitor.web.util.*;
+import com.ltmonitor.util.StringUtil;
+import org.apache.commons.lang.StringUtils;
+/**
+* 流量阈值配置表接口层
+*/
+@Controller
+@RequestMapping("/flowlimitconfig")
+@Slf4j
+public class FlowlimitconfigController  extends GenericAction{
+
+@Autowired
+private FlowlimitconfigService flowlimitconfigservice;
+
+@Autowired
+private EasyExceClasslUtil exceClasslUtil;
+
+
+/**
+* 保存流量阈值配置表 ,使用json请求，表单数据是解释含义，不需要传
+* @return
+*/
+@ResponseBody
+@RequestMapping("/save.action")
+public JsonMessage save(
+@RequestBody  Flowlimitconfig flowlimitconfig, HttpServletRequest request
+)
+throws Exception {
+flowlimitconfigservice.save(flowlimitconfig);
+return new JsonMessage(true, "操作成功");
+}
+
+
+/**
+* 删除流量阈值配置表
+* @param id 主键
+* @return
+*/
+@ResponseBody
+@RequestMapping("/delete.action")
+public JsonMessage delete(
+String id) throws Exception {
+flowlimitconfigservice.fdelete(id);
+return new JsonMessage(true, "操作成功");
+}
+
+/**
+* 查询列表
+        *   @param  id 主键
+        *   @param  name 名称
+        *   @param  startvalidstarttime 有效开始时间开始时间
+        *   @param  endvalidstarttime 有效开始时间结束时间
+        *   @param  startvalidendtime 有效结束时间开始时间
+        *   @param  endvalidendtime 有效结束时间结束时间
+        *   @param  startcreatedate 创建时间开始时间
+        *   @param  endcreatedate 创建时间结束时间
+        *   @param  startupdatedate 修改时间开始时间
+        *   @param  endupdatedate 修改时间结束时间
+        *   @param  userid 用户id
+        *   @param  deleted 删除标志,1代表删除,0代表正常
+* @return
+*/
+@ResponseBody
+@RequestMapping("/selectlist.action")
+public PaginateResult selectlist(
+/**
+*当前页
+*/
+@RequestParam(value = "page", defaultValue = "1") int page,
+/**
+*一页多少条
+*/
+@RequestParam(value = "rows", defaultValue = "10") int rows,
+/**
+*要进行查询的机构id,用逗号隔开
+*/
+@RequestParam(required = false) Long[] depIds,
+        /**
+        *主键
+        */
+        @RequestParam(required = false) Long  id,
+        /**
+        *名称
+        */
+        @RequestParam(required = false) String  name,
+        /**
+        *有效开始时间
+        */
+        @RequestParam(required = false) Date  startvalidstarttime,
+        @RequestParam(required = false) Date  endvalidstarttime,
+        /**
+        *有效结束时间
+        */
+        @RequestParam(required = false) Date  startvalidendtime,
+        @RequestParam(required = false) Date  endvalidendtime,
+        /**
+        *创建时间
+        */
+        @RequestParam(required = false) Date  startcreatedate,
+        @RequestParam(required = false) Date  endcreatedate,
+        /**
+        *修改时间
+        */
+        @RequestParam(required = false) Date  startupdatedate,
+        @RequestParam(required = false) Date  endupdatedate,
+        /**
+        *用户id
+        */
+        @RequestParam(required = false) Long  userid,
+        /**
+        *删除标志,1代表删除,0代表正常
+        */
+        @RequestParam(required = false) Long  deleted,
+HttpServletRequest request
+) {
+PaginateResult paginateResult = new PaginateResult(500, "服务器错误");
+try {
+Map params = getParams(request);
+OnlineUser onlineUser = getOnlineUser();
+if(onlineUser.isSuperAdmin()){
+params.put("owner", onlineUser.getEntityId());
+params.put("userid", onlineUser.getEntityId());
+}
+Uservehicleauthority uservehicleauthority=getUservehicleauthorityByWeb(depIds);
+quanxianUtil.getquanxian(uservehicleauthority, params);
+return flowlimitconfigservice.selectlist(params, page, rows);
+} catch (Exception e) {
+log.error("流量阈值配置表查询列表失败", e);
+return paginateResult;
+}
+}
+
+
+/**
+* 导出列表文件
+        *   @param  id 主键
+        *   @param  name 名称
+        *   @param  startvalidstarttime 有效开始时间开始时间
+        *   @param  endvalidstarttime 有效开始时间结束时间
+        *   @param  startvalidendtime 有效结束时间开始时间
+        *   @param  endvalidendtime 有效结束时间结束时间
+        *   @param  startcreatedate 创建时间开始时间
+        *   @param  endcreatedate 创建时间结束时间
+        *   @param  startupdatedate 修改时间开始时间
+        *   @param  endupdatedate 修改时间结束时间
+        *   @param  userid 用户id
+        *   @param  deleted 删除标志,1代表删除,0代表正常
+* @return
+*/
+@ResponseBody
+@RequestMapping("/exportlist.action")
+public void exportlist(
+/**
+*要进行查询的机构id,用逗号隔开
+*/
+@RequestParam(required = false) Long[] depIds,
+        /**
+        *主键
+        */
+        @RequestParam(value = "id",required = false) Long  id,
+        /**
+        *名称
+        */
+        @RequestParam(value = "name",required = false) String  name,
+        /**
+        *有效开始时间
+        */
+        @RequestParam(value = "validstarttime",required = false) Date  startvalidstarttime,
+        @RequestParam(value = "validstarttime",required = false) Date  endvalidstarttime,
+        /**
+        *有效结束时间
+        */
+        @RequestParam(value = "validendtime",required = false) Date  startvalidendtime,
+        @RequestParam(value = "validendtime",required = false) Date  endvalidendtime,
+        /**
+        *创建时间
+        */
+        @RequestParam(value = "createdate",required = false) Date  startcreatedate,
+        @RequestParam(value = "createdate",required = false) Date  endcreatedate,
+        /**
+        *修改时间
+        */
+        @RequestParam(value = "updatedate",required = false) Date  startupdatedate,
+        @RequestParam(value = "updatedate",required = false) Date  endupdatedate,
+        /**
+        *用户id
+        */
+        @RequestParam(value = "userid",required = false) Long  userid,
+        /**
+        *删除标志,1代表删除,0代表正常
+        */
+        @RequestParam(value = "deleted",required = false) Long  deleted,
+HttpServletRequest request,
+HttpServletResponse response
+) {
+try {
+Map params = getParams(request);
+Uservehicleauthority uservehicleauthority=getUservehicleauthorityByWeb(depIds);
+quanxianUtil.getquanxian(uservehicleauthority, params);
+PaginateResult selectmemberlist =flowlimitconfigservice.selectlist(params, 0, 0);
+List lists = selectmemberlist.getRows();
+List exldate = new ArrayList<>();
+for (int i = 0; i < lists.size(); i++) {
+Object row=lists.get(i);
+FlowlimitconfigExlVo flowlimitconfig = MaptoBeanUtil.mapToBean((Map
+<String, Object>) row, FlowlimitconfigExlVo.class);
+flowlimitconfig.setId(i+1);
+exldate.add(flowlimitconfig);
+}
+exceClasslUtil.exlport("流量阈值配置表", request, response, exldate, FlowlimitconfigExlVo.class);//调用easyexl模版进行导出exl特定标题的数据
+} catch (Exception e) {
+log.error("流量阈值配置表导出列表失败", e);
+}
+}
+
+
+private  Map getParams(HttpServletRequest request) {
+Map parameters = request.getParameterMap();
+Map paraMap = new HashMap();
+String rowdata = "";
+if (parameters.size() > 0) {
+for (Object key : parameters.keySet()) {
+String strKey = "" + key;
+int index = strKey.indexOf("[]");
+if (index > 0) {
+strKey = strKey.replaceAll("\\[\\]", "");
+}
+String[] values = (String[]) parameters.get(key);
+if (values.length == 1) {
+String strValue = values[0];
+if (strValue != null && strValue.isEmpty() == false)
+paraMap.put(strKey, strValue);
+} else
+paraMap.put(strKey, values);
+}
+} else {
+BufferedReader br = null;
+
+try {
+br = request.getReader();
+String line = br.readLine();
+if (line == null) {
+
+} else {
+StringBuilder ret = new StringBuilder();
+ret.append(line);
+
+while ((line = br.readLine()) != null) {
+ret.append('\n').append(line);
+}
+
+rowdata = ret.toString();
+}
+paraMap = (Map) JSON.parse(rowdata);
+} catch (IOException var4) {
+throw new RuntimeException(var4);
+}
+}
+return paraMap;
+}
+
+
+}
---
+++ src/main/resources/templatesdir/main/templatestree/flowlimitconfig/entity/Flowlimitconfigbydep.java
@@ -0,0 +1,49 @@
+package templatestree.flowlimitconfig.entity;
+
+import lombok.*;
+import net.fxft.common.jdbc.*;
+import java.util.*;
+
+@Data
+@DbTable(value="subiaodb.flowlimitconfigbydep")
+public class Flowlimitconfigbydep implements java.io.Serializable  {
+
+private static final long serialVersionUID = 1L;
+public static final String F_id = "id";
+public static final String F_mainid = "mainid";
+public static final String F_depid = "depid";
+public static final String F_createdate = "createdate";
+public static final String F_deleted = "deleted";
+
+
+/**  主键修改则传列表中的  */
+@DbId
+private Long  id;
+/**  配置表id修改则传列表中的  */
+private Long  mainid;
+/**  绑定机构的id修改则传列表中的  */
+private Long  depid;
+/**  创建时间修改则传列表中的  */
+private Date  createdate;
+/**  删除标志,0代表未删除,1代表删除修改则传列表中的  */
+private Long  deleted;
+
+public static void main(String[] args) {
+String name="{\n";
+name +="  \"id\":1, //主键\n";
+name +="  \"mainid\":1, //配置表id\n";
+name +="  \"depid\":1, //绑定机构的id\n";
+name +="  \"createdate\":\"2020-09-11 00:00:00\", //创建时间\n";
+name +="  \"deleted\":1 //删除标志,0代表未删除,1代表删除\n";
+name+="}";
+System.out.println(name);
+name ="    \"success\":true, \n "+
+" \"code\":200,\n " +
+" \"message\":\"success\", \n " +
+" \"data\": " +name+
+", \n \"total\":1 ";
+System.out.println(name);
+}
+
+
+}
