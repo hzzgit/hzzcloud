f43ec16e7f610210688484e34eae5d486902e118
hzzhome2 <605198347@qq.com>
2021-04-23 00:12:51 +0800

    无

--- src/main/java/com/hzz/hzzcloud/设计模式/a.java
+++ src/main/java/com/hzz/hzzcloud/设计模式/a.java
@@ -1,9 +1,18 @@
 package com.hzz.hzzcloud.设计模式;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+
 /**
  * @author ：hzz
  * @description：TODO
  * @date ：2020/7/28 9:53
  */
 public class a {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStreamReader inputStreamReader=new InputStreamReader(new FileInputStream(new File("d://TEST")));
+    }
 }
